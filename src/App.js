a4ded2f201504d574a7cc0b93b854afbe14e2f03
Imran Syed <imran@Imrans-MacBook-Air.local>
2022-08-23 08:45:29 -0500

    added form and third party api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [pokemon, setPokemon] = useState([]);
 
-const [search, setSearch] = useState("")
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
 
-const handleChange = (event) => {
-  setSearch(event.target.value)
-}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
-const handleSubmit = () => {
-  
-}
+  useEffect(()=>{
+
+    fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(response => response.json())
+    .then(data => setPokemon(data.results))
+}, [])
+
+  const displayPokemon = pokemon.map((item) => {
+    return (
+      <ul>
+        <li> {item.name}</li>
+      </ul>
+    );
+  });
 
   return (
     <div className="App">
-        
-      <form onSubmit = {handleSubmit}>
-          <input value = {search} onChange = {handleChange} type="text" />
-          <input type="submit" />
+      <form onSubmit={handleSubmit}>
+        <input value={search} onChange={handleChange} type="text" />
+        <input type="submit" />
       </form>
 
+      <div>{displayPokemon}</div>
     </div>
   );
 }
